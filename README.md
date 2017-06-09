97a3c8d05bd1c5236ec6071ae4178a2fdf5d28be
Dmitrii Sokolov <sokolovdp@gmail.com>
2017-06-09 18:58:36 +0200

    1st version

--- README.md
+++ README.md
@@ -1,17 +1,22 @@
 # Watcher of Friends Online
 
-[TODO. There will be project description]
+This program shows friends online of the given VK user
 
-# How to Install
-
-Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
-
-```bash
-pip install -r requirements.txt # alternatively try pip3
+usage: 
+------
 ```
+python vk_friends_online.py [--help] --user USER --token TOKEN
 
-Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
-
+Arguments:
+  --help         show this help message and exit
+  --user USER    user id or short 'screen name'
+  --token TOKEN  VK API access token
+```
+sample output:
+--------------
+```
+Катерина Ходакова has the following friends online: Иван Асютин, Ольга Теплова
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,93 @@
-import vk
+import requests
+import time
+import sys
+import argparse
+
+# from pprint import pprint
+
+VERSION = '5.65'  # VK API Version
+MAX_FRIENDS = 1000  # Max number of friends to analyse
+VK_TIMEOUT = 1.01  # Time in secs to pause when limit of requests is reached
+VK_REQ_LIMIT = 3  # Max number of requests to VK API per second
 
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+class VkUser:
+    def __init__(self, userid=0, token=0):
+        self.counter = 0
+        pars = {'access_token': token, 'v': VERSION, 'user_ids': [], 'fields': ['counters']}
+        data = self._get_response('https://api.vk.com/method/users.get', pars)
+        if not data:
+            print("invalid token: {}".format(token))
+            exit(4)
+        self.token = token
+        pars = {'access_token': token, 'v': VERSION, 'user_ids': [userid], 'fields': ['counters']}
+        data = self._get_response('https://api.vk.com/method/users.get', pars)
+        if not data:
+            print("invalid user id: {}".format(userid))
+            exit(5)
+        self.uid = data[0]['id']
+        self.name = data[0]['first_name'] + ' ' + data[0]['last_name']
+
+    def _get_response(self, url, par):
+        try:
+            data = requests.get(url, par).json()['response']
+        except ConnectionError:
+            print("error: VK connection problem")
+            exit(1)
+        except TimeoutError:
+            print("error: VK connection timeout")
+            exit(2)
+        except KeyError:
+            return {}
+        else:
+            if self.counter >= VK_REQ_LIMIT:  # check that there is not more than 3 operations per 1 sec
+                time.sleep(VK_TIMEOUT)
+                self.counter = 0
+            else:
+                self.counter += 1
+            return data
+
+    def get_friends_list(self):
+        pars = {'access_token': self.token, 'v': VERSION, 'user_id': self.uid, 'count': MAX_FRIENDS}
+        try:
+            return self._get_response('https://api.vk.com/method/friends.get', pars)['items']
+        except ValueError:
+            print("can't load friends list")
+
+    def get_main_user_name(self):
+        return self.name
+
+    def get_users_status(self, users_ids):
+        pars = {'access_token': self.token, 'v': VERSION, 'user_ids': "[{}]".format(",".join(map(str, users_ids))),
+                'fields': ['online']}
+        users_statuses = self._get_response('https://api.vk.com/method/users.get', pars)
+        return users_statuses
 
 
-def get_user_login():
-    pass
+def online_user(user_data):
+    return user_data['online']
 
 
-def get_user_password():
-    pass
+def get_online_friends(vkapi):
+    friends = vkapi.get_friends_list()
+    online_users_data = list(filter(online_user, vkapi.get_users_status(friends)))
+    return [user['first_name'] + " " + user['last_name'] for user in online_users_data]
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
-    api = vk.API(session)
-    # например, api.friends.get()
+def output_friends_to_console(username, friends_online):
+    print("\n{} has the following friends online: {}".format(username, ", ".join(friends_online)))
 
 
-def output_friends_to_console(friends_online):
-    pass
+def main(vkapi):
+    friends_online = get_online_friends(vkapi)
+    output_friends_to_console(vkapi.get_main_user_name(), friends_online)
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    ap = argparse.ArgumentParser(description='This program shows friends online of the given VK user')
+    ap.add_argument("--user", dest="user", action="store", required=True, help="  user id or short 'screen name'")
+    ap.add_argument("--token", dest="token", action="store", required=True, help="  VK API access token")
+
+    args = ap.parse_args(sys.argv[1:])
+
+    main(VkUser(userid=args.user, token=args.token))
